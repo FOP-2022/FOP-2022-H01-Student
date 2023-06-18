312bdce85ce77b1ca6666d871d459980db74d96e
tamoor1994 <73400071+tamoor1994@users.noreply.github.com>
2023-06-18 20:28:53 +0200

    Hausübung 01

--- src/main/java/h01/RookAndBishop.java
+++ src/main/java/h01/RookAndBishop.java
@@ -1,6 +1,9 @@
 package h01;
 
+import fopbot.Direction;
 import fopbot.Robot;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public class RookAndBishop {
   private final int NUMBER_OF_ROWS;
@@ -23,16 +26,75 @@ public class RookAndBishop {
     this(20, true);
   }
 
+  /**
+   * creates a value from 12 to 20
+   * @return coins between 12 to 20
+   */
+  public int randomCoins() {
+    int coins = ThreadLocalRandom.current().nextInt(12, 20);
+    return coins;
+  }
+
+  /**
+   * To produce a random number
+   * @return a number within the World
+   */
+  public int row() {
+    return ThreadLocalRandom.current().nextInt(NUMBER_OF_ROWS);
+  }
+
+  /**
+   * To produce a random number
+   * @return a number within the World
+   */
+  public int column() {
+    return ThreadLocalRandom.current().nextInt(NUMBER_OF_COLUMNS);
+  }
+
+  /**
+   * creates a radom value from 0 to 3
+   * @return on the base of the value the random Direction
+   */
+  public Direction randomDirection() {
+    int randomValue = ThreadLocalRandom.current().nextInt(0, 3);
+    Direction randomDirection = Direction.UP;
+
+    switch (randomValue) {
+      case 0:
+        randomDirection = Direction.UP;
+        break;
+      case 1:
+        randomDirection = Direction.RIGHT;
+        break;
+      case 2:
+        randomDirection = Direction.DOWN;
+        break;
+      case 3:
+        randomDirection = Direction.LEFT;
+        break;
+      default:
+        System.out.println("Fehler");
+    }
+
+    return randomDirection;
+  }
+
   public void execute() {
     Task1.initializeTask(NUMBER_OF_ROWS, NUMBER_OF_COLUMNS, nextFrameDelay, uiVisible);
-    // Hier programmieren
+
+    Robot rook = new Robot(row(), column(), randomDirection(), randomCoins());
+    Robot bishop = new Robot(row(), column(), randomDirection(), 0);
+    rookMovement(rook);
   }
 
   /**
    * Exercise 3.1 of H01
    */
   private void rookMovement(Robot rook) {
-    // Hier programmieren
+    rook.putCoin();
+    if (rook.isFrontClear()) {
+      rook.move();
+    }
   }
 
   /**
